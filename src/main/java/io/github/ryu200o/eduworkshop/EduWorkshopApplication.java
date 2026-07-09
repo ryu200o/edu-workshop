2786a697bd69996d406a9578a3cecd75d77a7812
Tech Lead (RYU-31) <tech-lead@workflow-optimize.local>
2026-07-09 14:25:11 +0700

    Initial commit

---
+++ src/main/java/io/github/ryu200o/eduworkshop/EduWorkshopApplication.java
@@ -0,0 +1,13 @@
+package io.github.ryu200o.eduworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ryu200o/eduworkshop/EduWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ryu200o.eduworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
